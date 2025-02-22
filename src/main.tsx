f7844a49f66f02cf0f5bdf4638055a63646daedf
TannerM-CTO <tanner@brightpathanalytics.com>
2025-02-22 16:54:50 -0600

    New fields for first and last name fr

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from "@aws-amplify/ui-react";
 import App from "./App.tsx";
 import "./index.css";
-import '@aws-amplify/ui-react/styles.css';
+import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
 import { parseAmplifyConfig } from "aws-amplify/utils";
 
 const amplifyConfig = parseAmplifyConfig(outputs);
@@ -19,9 +18,35 @@ Amplify.configure({
   },
 });
 
+// Define custom form fields for sign-up
+const formFields = {
+  signUp: {
+    given_name: {
+      label: "First Name",
+      placeholder: "Enter your first name",
+      isRequired: true,
+    },
+    family_name: {
+      label: "Last Name",
+      placeholder: "Enter your last name",
+      isRequired: true,
+    },
+    email: {
+      label: "Email",
+      placeholder: "Enter your email",
+      isRequired: true,
+    },
+    password: {
+      label: "Password",
+      placeholder: "Enter a strong password",
+      isRequired: true,
+    },
+  },
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator formFields={formFields}>
       <App />
     </Authenticator>
   </React.StrictMode>
